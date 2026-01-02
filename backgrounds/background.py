45f2c9bcd84ea512a7df9a1b51615804ad6539c2
qq939 <jiangjimjim@gmail.com>
2026-01-02 18:11:41 +0800

    面向对象ai？

---
+++ backgrounds/background.py
@@ -0,0 +1,7 @@
+class Background:
+    def __init__(self):
+        self.timestart = 0
+        self.name = "background1"
+        self.image = "backgroundImages/background1.png" # the directory path of the image
+        self.sound = "backgroundSounds/background1.mp3" # the directory path of the sound
+
---
+++ charectors/charector.py
@@ -0,0 +1,6 @@
+class Charector:
+    def __init__(self):
+        self.name = "char1"
+        self.image = "charectorImages/char1.png" # the directory path of the image
+        self.tone = "charectorTones/char1-tone.mp3" # the directory path of the tone
+
